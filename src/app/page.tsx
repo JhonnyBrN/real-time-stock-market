f08c88243834f6bfdbf665333e8c60bb52c61cd3
JhonnyBrN <146687470+JhonnyBrN@users.noreply.github.com>
2025-10-04 15:00:15 +0000

    Setup Inicial do Projeto

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button"
+
+const Page: () => React.JSX.Element = () => {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <Button>Click Me</Button>
+    </div>
+  )
+}
+
+export default Page
